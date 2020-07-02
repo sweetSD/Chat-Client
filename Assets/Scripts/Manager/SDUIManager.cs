a6089193bb3d6da11d79ebd0a301ed90b17cd17b
sweets125 <an060875@naver.com>
2020-07-03 00:57:04 +0900

    - 채팅 말풍선 제작

--- Assets/Scripts/Manager/SDUIManager.cs
+++ Assets/Scripts/Manager/SDUIManager.cs
@@ -5,19 +5,22 @@ using UnityEngine.UI;
 
 public class SDUIManager : SDSingleton<SDUIManager>
 {
-
+    [Header("Controller")]
     [SerializeField] private JoyStick _movementJoyStick;
     public JoyStick MovementJoyStick => _movementJoyStick;
 
     [SerializeField] private DragDetector _cameraControlDetector;
     public DragDetector CameraControlDetector => _cameraControlDetector;
 
+    [Header("Input Field")]
+    [SerializeField] private InputField _chatInputField;
+
+    [Header("Object Pools")]
     [SerializeField] private SDObjectPool _chatBoxPool;
     public SDObjectPool ChatBoxPool => _chatBoxPool;
 
-    [SerializeField] private InputField _chatInputField;
-
-    [SerializeField] private Image _image;
+    [SerializeField] private SDObjectPool _chatBubblePool;
+    public SDObjectPool ChatBubblePool => _chatBubblePool;
 
     private void Awake()
     {
@@ -31,23 +34,5 @@ public class SDUIManager : SDSingleton<SDUIManager>
                 _chatInputField.text = string.Empty;
             }
         });
-    }
-
-    /// <summary>
-    /// 말풍선 캐릭터 위치 생성 테스트
-    /// </summary>
-    public void test()
-    {
-        var rt = _image.canvas.GetComponent<RectTransform>();
-        var vp = Camera.main.WorldToViewportPoint(FindObjectOfType<NetworkMovement>().transform.position);
-        var sp = new Vector2(
-         ((vp.x * rt.sizeDelta.x) - (rt.sizeDelta.x * 0.5f)),
-         ((vp.y * rt.sizeDelta.y) - (rt.sizeDelta.y * 0.5f)));
-        _image.rectTransform.anchoredPosition = sp;
-    }
-
-    private void Update()
-    {
-        test();
     }
 }
---
+++ Assets/Scripts/UI/ChatBubble.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChatBubble : MonoBehaviour
+{
+    [SerializeField] private RectTransform _rectTransform;
+    private Transform _targetTransform;
+
+    private void Awake()
+    {
+        if (_rectTransform == null)
+            _rectTransform = GetComponent<RectTransform>();
+    }
+
+    public void Initialize(Transform targetTransform)
+    {
+        _targetTransform = targetTransform;
+    }
+
+    private void Update()
+    {
+        if (_targetTransform != null)
+        {
+            var vp = Camera.main.WorldToViewportPoint(_targetTransform.position);
+            var sp = new Vector2(
+             ((vp.x * _rectTransform.sizeDelta.x) - (_rectTransform.sizeDelta.x * 0.5f)),
+             ((vp.y * _rectTransform.sizeDelta.y) - (_rectTransform.sizeDelta.y * 0.5f)));
+            _rectTransform.anchoredPosition = sp;
+        }
+    }
+}
